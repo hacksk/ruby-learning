fdc1e581824c5af8508d08edf5e0081dcf7a0de4
Kiran S Kunjumon <kiran@disroot.org>
2019-08-08 16:22:16 +0530

    cleared white space

--- star.rb
+++ star.rb
@@ -1,4 +1,4 @@
 puts "enter an integer"
 a = gets.strip.to_i
-(1..a).each {|i| print ' ' * (a-i) + '*' * (2*i-1) + "\n" }
+(1..a).each {|i| print ' ' * (a-i) + '*' * (2*i-1) + "\n"}
 (1..(a-1)).each {|i| print ' ' * i + '*' * (2*(a-i)-1) + "\n"}
